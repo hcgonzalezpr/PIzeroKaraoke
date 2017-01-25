4854e6d1fce21a5fb71ed52bb8943552a70498b7
Harry Gonzalez <hcgonzalezpr@gmail.com>
2017-01-25 10:15:10 -0600

    Bit of code cleanup

--- html/index.php
+++ html/index.php
@@ -1,234 +1,189 @@
-<!DOCTYPE html> 
-<html> 
-<head> 
-	<title>PiZero Karaoke</title>
-
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
-
-	<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-
-	<script src="/jquery.base64.js"></script>
-
-</head> 
-<body> 
-
-<!-- Start of first page: #home -->
-<div data-role="page" id="home">
-
-	<div data-role="header">
-		<h3>PiZero Karaoke</h3>
-	</div><!-- /header -->
-
-	<div role="main" class="ui-content">
-		<p><a href="#songlist" data-role="button" data-transition="fade" data-icon="search" data-iconpos="right">Search for a song</a></p>
-		<p>
-		    <ul data-role="listview" data-inset="true">
-				<li><a href="#mainQueuePage" data-transition="fade">Current Queue<span class="ui-li-count">0</span></a></li>
-		    </ul>
-		</p>
-
-		<p data-role="button" data-theme="e" class="myMarquee">Now performing: Null </p>
-	</div><!-- /content -->
-	
-</div><!-- /page home -->
-
-<!-- Start of second page: #trackDialog -->
-<div data-role="page" id="trackDialog">
-
-	<div data-role="header">
-		<h1>Play this song next?</h1>
-	</div><!-- /header -->
-
-	<div data-role="content" data-theme="a">	
-		<h3 id="trackDialogArtistTitle"></h3>
-        <p><a href="#" onclick="window.queueSong();" data-role="button" data-theme="b" data-icon="plus" data-iconpos="right">Add to Queue</a></p>
-		<p><a href="#" onclick="window.playSong();" data-role="button" data-theme="b" >Play Now</a></p>
-        <p><a href="#" onclick="window.unselectTrack();" data-role="button" data-theme="a">Cancel</a></p>
-		
-	</div><!-- /content -->
-</div><!-- /page trackDialog -->
-
-<!-- Start of second page: #alertDialog -->
-<div data-role="page" id="alertDialog">
-
-	<div data-role="header" data-position="fixed">
-		<h2 id="alertDialogMessage"></h2>
-	</div><!-- /header -->
-
-	<div data-role="content" data-theme="a">	
-        <p><a href="#" onclick="window.closeDialog();" data-role="button" data-theme="b">OK</a></p>
-		
-	</div><!-- /content -->
-</div><!-- /page alertDialog -->
-
-<!-- Start of third page: #mainQueuePage -->
-<div data-role="page" id="mainQueuePage" >
-
-	<div data-role="header">
-		<h1>Current Queue</h1>
-		<a href="#home" class="ui-btn-right" data-icon="home" data-iconpos="notext" data-rel="back">Home</a>
-	</div><!-- /header -->
-
-	<div data-role="content">	
-		<p>Who's up next! Get ready!</p>
-		<p>
-		    <ol id="mainQueueListView" data-role="listview" data-inset="true">
-		    </ol>
-		</p>
-	</div><!-- /content -->
-
-</div><!-- /page popup -->
-
-
-
-<!-- Start of fourth page: #songlist -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PiZero Karaoke</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css"/>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+    <script src="/jquery.base64.js"></script>
+</head>
+<body>
 <?php
 // Path
 $dir = "/home/pi/songs/";
-
-$files = glob($dir.'*.mp3');
-
+$files = glob($dir . '*.mp3');
 $ar = null; ?>
-<div data-role="page" id="songlist">
 
+<div data-role="page" id="home">
     <div data-role="header">
-        <h1>Songs List <small>( <?php echo count($files); ?> songs )</small></h1>
+        <h3>PiZero Karaoke</h3>
+    </div>
+    <div role="main" class="ui-content">
+        <p><a href="#songlist" data-role="button" data-transition="fade" data-icon="search" data-iconpos="right">Search
+                for a song</a></p>
+        <p>
+        <ul data-role="listview" data-inset="true">
+            <li><a href="#mainQueuePage" data-transition="fade">Current Queue<span class="ui-li-count">0</span></a></li>
+        </ul>
+        </p>
+        <p data-role="button" data-theme="e" class="myMarquee">Now performing: Null </p>
+    </div>
+</div>
+
+<div data-role="page" id="trackDialog">
+    <div data-role="header">
+        <h1>Play this song next?</h1>
+    </div>
+    <div data-role="content">
+        <h3 id="trackDialogArtistTitle"></h3>
+        <p><a href="#" onclick="window.queueSong();" data-role="button" data-theme="b" data-icon="plus"
+              data-iconpos="right">Add to Queue</a></p>
+        <p><a href="#" onclick="window.playSong();" data-role="button" data-theme="b">Play Now</a></p>
+        <p><a href="#" onclick="window.closeDialog();" data-role="button" data-theme="a">Cancel</a></p>
+    </div>
+</div>
+
+<div data-role="page" id="alertDialog">
+    <div data-role="header">
+        <h2 id="alertDialogMessage"></h2>
+    </div>
+    <div data-role="content">
+        <p><a href="#" onclick="window.closeDialog();" data-role="button" data-theme="b">OK</a></p>
+    </div>
+</div>
+
+<div data-role="page" id="mainQueuePage">
+    <div data-role="header">
+        <h1>Current Queue</h1>
+        <a href="#home" class="ui-btn-right" data-icon="home" data-iconpos="notext" data-rel="back">Home</a>
+    </div>
+    <div data-role="content">
+        <p>Who's up next! Get ready!</p>
+        <p>
+        <ol id="mainQueueListView" data-role="listview" data-inset="true">
+        </ol>
+        </p>
+    </div>
+</div>
+
+<div data-role="page" id="songlist">
+    <div data-role="header">
+        <h1>Songs List
+            <small>( <?php echo count($files); ?> songs )</small>
+        </h1>
         <a class="ui-btn-right" data-icon="home" data-iconpos="notext" data-rel="back">Home</a>
     </div>
     <div data-role="content">
-        <input data-type="search" id="searchFieldA" placeholder="Search for a song" />
-		<ul id="suggestionsa" data-role="listview" data-inset="true" data-input="#searchFieldA" data-filter="true">
-		 <?php
+        <input data-type="search" id="searchFieldA" placeholder="Search for a song"/>
+        <ul id="suggestionsa" data-role="listview" data-inset="true" data-input="#searchFieldA" data-filter="true"><?php
 
-		foreach ($files as $file) {
+            foreach ($files as $file) {
 
-			$filename = pathinfo($file, PATHINFO_FILENAME);
-			$songinfo = explode(" - ", $filename);
+                $filename = pathinfo($file, PATHINFO_FILENAME);
+                $songinfo = explode(" - ", $filename);
 
-			$artist = $songinfo[0];
-			$song = $songinfo[1];
+                $artist = $songinfo[0];
+                $song = $songinfo[1];
 
-			if ($ar !== $artist) {
-				echo '<li data-role="list-divider">'.$artist.'</li>';
-				$ar = $artist;
-			}
+                if ($ar !== $artist) {
+                    echo '<li data-role="list-divider">' . $artist . '</li>';
+                    $ar = $artist;
+                }
 
-			echo '<li data-filtertext="'.$artist.':'.$song.'" ><a href="#trackDialog" data-artist-title="'.$filename.'" onclick="selectTrack(\''.$file.'\');" data-rel="dialog" data-transition="pop">'.$song.'</a></li>';
-		}
-		?>
-		</ul>
+                echo '<li data-filtertext="' . $artist . ':' . $song . '" ><a href="#trackDialog" data-artist-title="' . $filename . '" onclick="selectTrack(\'' . $file . '\');" data-rel="dialog" data-transition="pop">' . $song . '</a></li>';
+            }
+            ?></ul>
     </div>
-
 </div>
 
 <script>
-(function($){
+    (function ($) {
 
-	window.playSong = function(path_to_file){
-		var path = path_to_file || sessionStorage.selectedTrack;
+        window.playSong = function (path_to_file) {
+            var path = path_to_file || sessionStorage.selectedTrack;
 
-		$.getJSON('/s.php?p=' + $.base64.encode(path), function(data){
-			//console.log(data);
-			$('.ui-dialog').dialog('close');
-		});
-	};
+            $.getJSON('/s.php?p=' + $.base64.encode(path), function (data) {
+                $('.ui-dialog').dialog('close');
+            });
+        };
 
-	window.queueSong = function(path_to_file){
+        window.queueSong = function (path_to_file) {
 
-			var path = path_to_file || sessionStorage.selectedTrack;
+            var path = path_to_file || sessionStorage.selectedTrack;
 
-			$.getJSON('/s.php?aq=' + $.base64.encode(path), function(data){
-				if(data.result === "OK"){
-					//console.log(data);
-					
-					//window.closeDialog();
-					$('.ui-dialog').dialog('close');
-					window.setTimeout(function(){
-						window.showAlertDialog("Song added to Queue");
-					},500);
-				}
-			});	
+            $.getJSON('/s.php?aq=' + $.base64.encode(path), function (data) {
+                if (data.result === "OK") {
 
-	};
+                    $('.ui-dialog').dialog('close');
+                    window.setTimeout(function () {
+                        window.showAlertDialog("Song added to Queue");
+                    }, 500);
+                }
+            });
+        };
 
-	window.selectTrack = function(file_path){
-		//remember path but enhance in the futre to know the user's id to submit a performance
-		sessionStorage.selectedTrack=file_path;
-	};
-	window.unselectTrack = function(){
-		sessionStorage.removeItem("selectedTrack");
-		window.closeDialog();
-	};
-	window.addToMyQueue = function(track_id){
-		//calls server-side rest
-	};
-	window.addToFavorites = function(track_id){
-		//calls server-side rest
-	};
-	window.cancelSelectedTrack = function(track_id){
-		//do we need this function?
-	};
+        window.selectTrack = function (file_path) {
+            //remember path but enhance in the futre to know the user's id to submit a performance
+            sessionStorage.selectedTrack = file_path;
+        };
 
-	window.showAlertDialog = function(message){
+        window.showAlertDialog = function (message) {
 
-		$('#alertDialogMessage').text(message);
+            $('#alertDialogMessage').text(message);
 
-		$.mobile.changePage( $('#alertDialog'), {role: 'dialog', transition: 'pop'});
+            $.mobile.changePage($('#alertDialog'), {role: 'dialog', transition: 'pop'});
 
-	};
+        };
 
-	window.updateMainQueueListView = function() {
+        window.closeDialog = function () {
+            $('.ui-dialog').dialog('close');
+        };
 
-		$.ajax({
-			type: "GET",
-			url: "/s.php?pl",
-			dataType: "xml",
-			success: xmlParser
-		});
+        window.updateMainQueueListView = function () {
 
-	};
+            $.ajax({
+                type: "GET",
+                url: "/s.php?pl",
+                dataType: "xml",
+                success: xmlParser
+            });
 
-	window.closeDialog = function(){
-		$('.ui-dialog').dialog('close');
-	};
+        };
 
-	function xmlParser(xml) {
+        function xmlParser(xml) {
 
-		var html = [];
+            var html = [];
 
-		$($(xml).find('leaf').reverse()).each(function() {
+            $($(xml).find('leaf').reverse()).each(function () {
 
-			var uri = $(this).attr('uri');
-			var Filename = uri.split('/').pop().slice(0, -4);
-			var songinfo = Filename.split('-');
+                var uri = $(this).attr('uri');
+                var Filename = uri.split('/').pop().slice(0, -4);
+                var songinfo = Filename.split('-');
 
-			if (songinfo[1]) {
-				html.push('<li><a href="#"><h2>' + songinfo[1] + '</h2><p>' + songinfo[0] + '</p></a></li>');
-			}
+                if (songinfo[1]) {
+                    html.push('<li><a href="#"><h2>' + songinfo[1] + '</h2><p>' + songinfo[0] + '</p></a></li>');
+                }
 
-		});
+            });
 
-		var lv = $('#mainQueueListView');
-		lv.html(html.join(''));
-		lv.listview("refresh");
+            var lv = $('#mainQueueListView');
+            lv.html(html.join(''));
+            lv.listview("refresh");
 
-	};
+        };
 
-	$('#home').on("pageshow", function(e){
-	});
+        $('#home').on("pageshow", function (e) {
+        });
 
-	$("#mainQueuePage").on("pagebeforeshow", function(e){
-		window.updateMainQueueListView();
-	});
+        $("#mainQueuePage").on("pagebeforeshow", function (e) {
+            window.updateMainQueueListView();
+        });
 
-	// Reverse trick
-	jQuery.fn.reverse = [].reverse;
+        // Reverse trick
+        jQuery.fn.reverse = [].reverse;
 
-})(jQuery);
-
+    })(jQuery);
 </script>
 </body>
 </html>
--- html/s.php
+++ html/s.php
@@ -1,184 +1,39 @@
 <?php
-
 // Simple Karaoke Server Backend
-
-error_reporting(E_ALL);
-
-// Path
-$dir = "/home/pi/songs/";
-
-if (!is_null($_GET["sa"])) {
-
-$files = glob($dir.'*.mp3');
-
-	foreach ($files as $file) {
-
-	$f['name'] = pathinfo($file, PATHINFO_FILENAME);
-
-	$songinfo = explode(" - ", $f['name']);
-
-	$f['artist'] = $songinfo[0];
-
-	$track['t'] = $songinfo[1];
-	$track['fp'] = $file;
-
-	$f['track'] = $track;
-
-	$songlist[] = $f;
-
-	}
-
-
-$filtered = array_filter($songlist, function($a) {
-$pattern = "/^" . $_GET["sa"] . "/i";
-return preg_match($pattern, $a['name']);});    
-
-$res['results'] = array_values($filtered);
-$res['keyword'] = $_GET["sa"];
-$res['length'] = count($res['results']);
-
-echo json_encode($res);
-
-}
-
-if (!is_null($_GET["ss"])) {
-
-$files = glob($dir.'*.mp3');
-
-foreach ($files as $file) {
-
-	$f['name'] = pathinfo($file, PATHINFO_FILENAME);
-
-	$songinfo = explode(" - ", $f['name']);
-
-	$f['artist'] = $songinfo[0];
-
-	$track['t'] = $songinfo[1];
-	$track['fp'] = $file;
-
-	$f['track'] = $track;
-
-	$songlist[] = $f;
-
-}
-
-
-$filtered = array_filter($songlist, function($a) {
-$pattern = "/- " . $_GET["ss"] . "/i";
-return preg_match($pattern, $a['name']);});    
-
-$res['results'] = array_values($filtered);
-$res['keyword'] = $_GET["ss"];
-$res['length'] = count($res['results']);
-
-echo json_encode($res);
-
-}
-
-if (!is_null($_GET["sn"])) {
-
-$files = glob($dir.'*.mp3');
-
-	foreach ($files as $file) {
-
-	$f['name'] = pathinfo($file, PATHINFO_FILENAME);
-
-	$songinfo = explode(" - ", $f['name']);
-
-	$f['artist'] = $songinfo[0];
-
-	$track['t'] = $songinfo[1];
-	$track['fp'] = $file;
-
-	$f['track'] = $track;
-
-	$songlist[] = $f;
-
-	}
-
-
-$filtered = array_filter($songlist, function($a) {
-$pattern = "/" . $_GET["sn"] . "/i";
-return preg_match($pattern, $a['name']);});    
-
-$res['results'] = array_values($filtered);
-$res['keyword'] = $_GET["sn"];
-$res['length'] = count($res['results']);
-
-echo json_encode($res);
-
-}
-
-if (!is_null($_GET["aq"])) {
-
-$context = stream_context_create(array(
+$auth = stream_context_create(array(
     'http' => array(
-        'header'  => "Authorization: Basic " . base64_encode(":vlc")
+        'header' => "Authorization: Basic " . base64_encode(":vlc")
     )
 ));
 
-$url = 'http://localhost:8080/requests/status.xml?command=in_enqueue&input=file://' . rawurlencode(base64_decode($_GET["aq"]));
+if (!is_null($_GET["aq"])) {
 
-$data = file_get_contents($url, false, $context);
+    $url = 'http://localhost:8080/requests/status.xml?command=in_enqueue&input=file://' . rawurlencode(base64_decode($_GET["aq"]));
 
-$ok['result'] = 'OK';
-echo json_encode($ok);
+    $data = file_get_contents($url, false, $auth);
+
+    $ok['result'] = 'OK';
+    echo json_encode($ok);
 
 }
 
 if (!is_null($_GET["p"])) {
 
-$context = stream_context_create(array(
-    'http' => array(
-        'header'  => "Authorization: Basic " . base64_encode(":vlc")
-    )
-));
+    $url = 'http://localhost:8080/requests/status.xml?command=in_play&input=file://' . rawurlencode(base64_decode($_GET["p"]));
 
-$url = 'http://localhost:8080/requests/status.xml?command=in_play&input=file://' . rawurlencode(base64_decode($_GET["p"]));
+    $data = file_get_contents($url, false, $auth);
 
-$data = file_get_contents($url, false, $context);
-
-$ok['result'] = 'OK';
-echo json_encode($ok);
+    $ok['result'] = 'OK';
+    echo json_encode($ok);
 
 }
 
 if (!is_null($_GET["pl"])) {
 
-    $context = stream_context_create(array(
-        'http' => array(
-            'header'  => "Authorization: Basic " . base64_encode(":vlc")
-        )
-    ));
-
     $url = 'http://localhost:8080/requests/playlist.xml';
 
-    $data = file_get_contents($url, false, $context);
+    $data = file_get_contents($url, false, $auth);
 
-   echo $data;
-
-}
-
-
-if (!is_null($_GET["cu"])) {
-
-$context = stream_context_create(array(
-    'http' => array(
-        'header'  => "Authorization: Basic " . base64_encode(":vlc")
-    )
-));
-
-$url = 'http://localhost:8080/requests/playlist.xml';
-
-$data = file_get_contents($url, false, $context);
-
-$xml = new SimpleXMLElement($data);
-
-json_encode($xml->node[0]);
-
-$ok['result'] = 'OK';
-echo json_encode($ok);
+    echo $data;
 
 }
-
-
